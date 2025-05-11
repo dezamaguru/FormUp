1debfb583514c8410bb1a8dc0dccb3ab6ed1a9d5
dezamaguru <budia.iulia@gmail.com>
2025-05-11 14:36:23 +0300

    Functionalitate de mesagerie intre studenti si secretariat

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Route, Routes} from 'react-router-dom';
 import Login from '../components/Login/Login';
 import Home from '../components/Home/Home';
-import AddUser from '../components/AddUser';
+import AddUser from '../components/Users/AddUser';
 import Users from '../components/Users/Users';
 import Layout from '../components/Layout';
 import RequireAuth from '../components/RequireAuth';
